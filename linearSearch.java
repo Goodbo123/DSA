5ef9e8a34ce5edf5f1d5cac396f3d97d33f7a561
Goodbo123 <100330857+Goodbo123@users.noreply.github.com>
2024-03-26 10:46:17 +0530

    Update linearSearch.java

--- linearSearch.java
+++ linearSearch.java
@@ -1,1 +1,28 @@
+public class linearSearch {
+    public static void main(String arg[]){
+        int nums[]={1,2,3,4,5,6,7,8};
+        int target=6;
+
+        int result=linear_search(nums, target);
+        if(result!=-1)
+        {
+            System.out.println("Element found at index: "+result);
+        }
+        else
+        {
+            System.out.println("Element not found!");
+        }
+
+    }
+
+    public static int linear_search(int nums[], int target){
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
 
